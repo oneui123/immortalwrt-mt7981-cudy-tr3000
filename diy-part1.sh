1510069255cea8ab372ce581d7989a9e245f291e
船长 <Hi-Moka@outlook.com>
2026-01-02 20:12:58 +0800

    Remove Aurora theme
    Removed cloning of luci-theme-aurora and luci-app-aurora-config.

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
