c6c7c2a69d8ae29a1e8c82f08e277e8a5e3ca625
船长 <Hi-Moka@outlook.com>
2026-01-18 17:27:38 +0800

    Implement latest mihomo core installation for OpenClash
    Added functionality to download and install the latest stable release of the mihomo core for OpenClash, including backup comments for previous installation steps.

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,49 @@ sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
 # Add OpenClash Meta
-mkdir -p files/etc/openclash/core
 
-wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
-mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
-chmod +x files/etc/openclash/core/clash_meta
-rm -f "clash_meta.tar.gz"
+# # # backup start
+# mkdir -p files/etc/openclash/core
+
+# wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+# tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+# mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+# chmod +x files/etc/openclash/core/clash_meta
+# rm -f "clash_meta.tar.gz"
+# # # backup end
+
+# -------------------------------------------------------
+# Add OpenClash Meta (mihomo) - Latest Stable ARM64
+# -------------------------------------------------------
+
+set -e
+
+CORE_DIR="files/etc/openclash/core"
+TMP_FILE="/tmp/mihomo_latest.tar.gz"
+
+mkdir -p ${CORE_DIR}
+
+echo "Fetching latest mihomo stable release tag..."
+
+LATEST_TAG=$(wget -qO- https://api.github.com/repos/MetaCubeX/mihomo/releases/latest | \
+             grep '"tag_name"' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')
+
+echo "Latest mihomo version: ${LATEST_TAG}"
+
+DOWNLOAD_URL="https://github.com/MetaCubeX/mihomo/releases/download/${LATEST_TAG}/mihomo-linux-arm64-${LATEST_TAG}.tar.gz"
+
+echo "Downloading: ${DOWNLOAD_URL}"
+
+wget -qO "${TMP_FILE}" "${DOWNLOAD_URL}"
+
+tar -zxvf "${TMP_FILE}" -C "${CORE_DIR}"
+
+# 统一 OpenClash 识别的内核名称
+if [ -f "${CORE_DIR}/mihomo" ]; then
+    mv "${CORE_DIR}/mihomo" "${CORE_DIR}/clash_meta"
+fi
+
+chmod +x "${CORE_DIR}/clash_meta"
+rm -f "${TMP_FILE}"
+
+echo "mihomo stable core installed successfully."
