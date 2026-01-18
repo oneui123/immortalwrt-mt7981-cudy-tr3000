398014aee6bab921097e77dfdbaf3aa0c0b93694
船长 <Hi-Moka@outlook.com>
2026-01-18 21:52:02 +0800

    Refactor OpenClash Meta installation in diy-part2.sh
    Updated OpenClash Meta installation process by removing outdated comments and commands.

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,33 +26,15 @@ sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
 # set ubi to 122M
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
+# -------------------------------------------------------
 # Add OpenClash Meta
-
-# # # backup start
-# mkdir -p files/etc/openclash/core
-
-# wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-# tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
-# mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
-# chmod +x files/etc/openclash/core/clash_meta
-# rm -f "clash_meta.tar.gz"
-# # # backup end
-
-# -------------------------------------------------------
-# Add OpenClash Meta (mihomo) - Latest Stable ARM64
 # -------------------------------------------------------
 
-# 创建 OpenClash core 目录
+
 mkdir -p files/etc/openclash/core
 
-# 获取 mihomo 最新稳定版下载地址（ARM64）
-MIHOMO_URL=$(wget -qO- https://api.github.com/repos/MetaCubeX/mihomo/releases/latest \
-  | grep browser_download_url \
-  | grep linux-arm64 \
-  | cut -d '"' -f 4)
-
-# 下载 mihomo
-wget -qO files/etc/openclash/core/clash_meta "$MIHOMO_URL"
-
-# 赋予执行权限
+wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
 chmod +x files/etc/openclash/core/clash_meta
+rm -f "clash_meta.tar.gz"
