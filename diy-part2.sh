1009c231ced30427dbf3cd50023e3c9ccd7bf3dc
船长 <Hi-Moka@outlook.com>
2026-01-26 22:26:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,7 @@ sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
 # set ubi to 122M
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
-# --------Add OpenClash Meta --------
+# -------- 添加 OpenClash Meta --------
 mkdir -p files/etc/openclash/core
 
 wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
@@ -37,30 +37,22 @@ rm -f "clash_meta.tar.gz"
 
 # -------- 添加 PassWall 核心 --------
 # 创建存储二进制文件的目录
-BIN_DIR="$GITHUB_WORKSPACE/openwrt/files/usr/bin"
-mkdir -p "$BIN_DIR"
+# BIN_DIR="$GITHUB_WORKSPACE/openwrt/files/usr/bin"
+# mkdir -p "$BIN_DIR"
 
 # -------- 下载并解压 xray-core ARM64 -------
-echo "Downloading xray-core..."
-curl -L -o xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.1.23/Xray-linux-arm64-v8a.zip
-unzip -o xray.zip -d "$BIN_DIR"
-chmod +x "$BIN_DIR/xray"
-rm xray.zip
+# echo "Downloading xray-core..."
+# curl -L -o xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.1.23/Xray-linux-arm64-v8a.zip
+# unzip -o xray.zip -d "$BIN_DIR"
+# chmod +x "$BIN_DIR/xray"
+# rm xray.zip
 
 # -------- 下载并解压 sing-box ARM64 -------
-echo "Downloading sing-box..."
-curl -L -o sing-box.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.12.17/sing-box-1.12.17-linux-arm64.tar.gz
-TMP_DIR=$(mktemp -d)
-tar -xzf sing-box.tar.gz -C "$TMP_DIR"
-mv "$TMP_DIR"/sing-box-1.12.17-linux-arm64/sing-box "$BIN_DIR"/sing-box
-chmod +x "$BIN_DIR/sing-box"
-rm -rf "$TMP_DIR"
-rm sing-box.tar.gz
-
-# -------- 下载 Geoview 最新版 --------
-echo "Downloading geoview binary..."
-BIN_DIR="$GITHUB_WORKSPACE/openwrt/files/usr/bin"
-mkdir -p "$BIN_DIR"
-curl -L -o "$BIN_DIR/geoview" \
-  "https://github.com/snowie2000/geoview/releases/download/v0.2.5/geoview-linux-arm64"
-chmod +x "$BIN_DIR/geoview"
+# echo "Downloading sing-box..."
+# curl -L -o sing-box.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.12.17/sing-box-1.12.17-linux-arm64.tar.gz
+# TMP_DIR=$(mktemp -d)
+# tar -xzf sing-box.tar.gz -C "$TMP_DIR"
+# mv "$TMP_DIR"/sing-box-1.12.17-linux-arm64/sing-box "$BIN_DIR"/sing-box
+# chmod +x "$BIN_DIR/sing-box"
+# rm -rf "$TMP_DIR"
+# rm sing-box.tar.gz
